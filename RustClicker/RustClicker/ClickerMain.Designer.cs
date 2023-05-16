daff6012a20f7391026841b0252ca4e89bf7b531
Arda Eren <70160402+DeCEll-1@users.noreply.github.com>
2023-05-16 23:45:07 +0300

    s

--- RustClicker/RustClicker/ClickerMain.Designer.cs
+++ RustClicker/RustClicker/ClickerMain.Designer.cs
@@ -32,6 +32,9 @@
             this.pb_Rustman12 = new System.Windows.Forms.ProgressBar();
             this.btn_Money = new System.Windows.Forms.Button();
             this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_EverySecond = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_EverySecondCount = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -46,6 +49,7 @@
             // 
             // pb_Rustman12
             // 
+            this.pb_Rustman12.AccessibleDescription = "s";
             this.pb_Rustman12.Location = new System.Drawing.Point(18, 121);
             this.pb_Rustman12.MarqueeAnimationSpeed = 200;
             this.pb_Rustman12.Maximum = 110;
@@ -74,11 +78,45 @@
             this.btn_Save.UseVisualStyleBackColor = true;
             this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
             // 
+            // btn_EverySecond
+            // 
+            this.btn_EverySecond.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn_EverySecond.Location = new System.Drawing.Point(18, 150);
+            this.btn_EverySecond.Name = "btn_EverySecond";
+            this.btn_EverySecond.Size = new System.Drawing.Size(136, 54);
+            this.btn_EverySecond.TabIndex = 6;
+            this.btn_EverySecond.Text = "Buy rustman12 for: \r\n";
+            this.btn_EverySecond.UseVisualStyleBackColor = true;
+            this.btn_EverySecond.Click += new System.EventHandler(this.btn_EverySecond_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(160, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "x/s";
+            // 
+            // lbl_EverySecondCount
+            // 
+            this.lbl_EverySecondCount.AutoSize = true;
+            this.lbl_EverySecondCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lbl_EverySecondCount.Location = new System.Drawing.Point(160, 118);
+            this.lbl_EverySecondCount.Name = "lbl_EverySecondCount";
+            this.lbl_EverySecondCount.Size = new System.Drawing.Size(60, 24);
+            this.lbl_EverySecondCount.TabIndex = 9;
+            this.lbl_EverySecondCount.Text = "label3";
+            // 
             // ClickerMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_EverySecondCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_EverySecond);
             this.Controls.Add(this.btn_Save);
             this.Controls.Add(this.btn_Money);
             this.Controls.Add(this.pb_Rustman12);
@@ -96,5 +134,8 @@
         private System.Windows.Forms.ProgressBar pb_Rustman12;
         private System.Windows.Forms.Button btn_Money;
         private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_EverySecond;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_EverySecondCount;
     }
 }
--- RustClicker/RustClicker/ClickerMain.cs
+++ RustClicker/RustClicker/ClickerMain.cs
@@ -14,6 +14,55 @@ namespace RustClicker
 {
     public partial class ClickerMain : Form
     {
+        public ClickerMain()
+        {
+            InitializeComponent();
+
+            Statics.db = ss.XMLDeserialiserWS();
+
+            if (Statics.db == null)
+            {
+                BaseClass dbs = new BaseClass();
+                Statics.db = dbs;
+            }
+
+            if (Statics.db.BigNumberFuniClickMultiplier == 0)
+            {
+                Statics.db.BigNumberFuniClickMultiplier++;
+            }
+
+            lbl_EverySecondCount.Text = Statics.db.EverySecond.ToString();
+
+
+            Reset();
+
+
+
+            InitTimer();
+
+        }
+
+        public void Reset()
+        {
+            if (Statics.db.EverySecond != 0)
+            {
+
+                int s = 10;
+
+                for (int i = 0; i <= Statics.db.EverySecond; i++)
+                {
+                    s = Convert.ToInt32(s * 1.5);
+                }
+
+                btn_EverySecond.Text = "Buy rustman12 for:\n" + s;
+
+            }
+            else
+            {
+                btn_EverySecond.Text = "Buy rustman12 for:\n" + 10;
+            }
+        }
+
         SerilisationStuff ss = new SerilisationStuff();
 
         public int IntervalThing { get; set; }
@@ -48,26 +97,6 @@ namespace RustClicker
         }
 
 
-        public ClickerMain()
-        {
-            InitializeComponent();
-
-            Statics.db = ss.XMLDeserialiserWS();
-
-            if (Statics.db == null)
-            {
-                BaseClass dbs = new BaseClass();
-                Statics.db = dbs;
-            }
-
-            if (Statics.db.BigNumberFuniClickMultiplier == 0)
-            {
-                Statics.db.BigNumberFuniClickMultiplier++;
-            }
-
-            Statics.db.EverySecond++;
-            InitTimer();
-        }
 
 
         private void btn_Money_Click(object sender, EventArgs e)
@@ -78,7 +107,14 @@ namespace RustClicker
 
         private void btn_Save_Click(object sender, EventArgs e)
         {
-            ss.Save(Statics.db);
+            try
+            {
+                ss.Save(Statics.db);
+            }
+            catch (Exception)
+            {
+                ss.Save(Statics.db);
+            }
         }
 
         private void ClickerMain_FormClosing(object sender, FormClosingEventArgs e)
@@ -89,8 +125,51 @@ namespace RustClicker
             }
             catch (Exception)
             {
+                ss.Save(Statics.db);
             }
-            ss.Save(Statics.db);
+        }
+
+        private void btn_EverySecond_Click(object sender, EventArgs e)
+        {
+            if (Statics.db.EverySecond == 0)
+            {
+                if (Statics.db.BigNumberFuni >= 10)
+                {
+                    Statics.db.EverySecond++;
+                    Statics.db.BigNumberFuni -= 10;
+                    btn_Money.Text = Statics.db.BigNumberFuni.ToString();
+
+                    lbl_EverySecondCount.Text = Statics.db.EverySecond.ToString();
+
+                    btn_Save_Click(null, null);
+
+                    Reset();
+
+                }
+            }
+            else
+            {
+                int s = 10;
+
+                for (int i = 0; i <= Statics.db.EverySecond; i++)
+                {
+                    s = Convert.ToInt32(s * 1.5);
+                }
+
+                if (Statics.db.BigNumberFuni >= (ulong)s)
+                {
+                    Statics.db.EverySecond++;
+                    Statics.db.BigNumberFuni -= (ulong)s;
+                    btn_Money.Text = Statics.db.BigNumberFuni.ToString();
+
+                    lbl_EverySecondCount.Text = Statics.db.EverySecond.ToString();
+
+                    btn_Save_Click(null, null);
+
+                    Reset();
+
+                }
+            }
         }
     }
 }
